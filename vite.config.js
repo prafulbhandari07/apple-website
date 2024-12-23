f7af5c00317e21fd559185d006ee1f1e4458e955
Praful Bhandari <pbhandari490@gmail.com>
2024-12-23 17:57:27 +0530

    Update vite.config.js
    reduce chunking

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [
+            "react",
+            "react-dom",
+            "three",
+            "@react-three/fiber",
+            "@react-three/drei",
+          ],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1500, // Increase the limit (in KB) to suppress the warning
+  },
+});
